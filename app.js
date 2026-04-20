9f2f66822f1849ed87df5c865ec8dcf70bd23856
Milos Dundjerovic <71335364+djandooLink@users.noreply.github.com>
2026-04-21 01:45:06 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+
+const textElement = document.querySelector('.typing-text');
+
+const words = ["Web Developer", "Frontend Developer", "React Developer"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+    const currentWord = words[wordIndex];
+
+    if (isDeleting) {
+        charIndex--;
+    } else {
+        charIndex++;
+    }
+
+    textElement.textContent = currentWord.substring(0, charIndex);
+
+    let speed = isDeleting ? 60 : 120;
+
+    if (!isDeleting && charIndex === currentWord.length) {
+        speed = 1200; // pauza kad se ispise
+        isDeleting = true;
+    }
+
+    if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+        speed = 400;
+    }
+
+    setTimeout(typeEffect, speed);
+}
+
+typeEffect();
+
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+});
